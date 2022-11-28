86be194e3920eece9f4221c0258824940f8859fa
IMythx <88852348+IMythx@users.noreply.github.com>
2022-11-28 21:08:50 +0200

    updated API KEY

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let selectMenuOptions = document.querySelectorAll(
   "header .container .search .type .options li"
 );
 let searchButton = document.querySelector("header .container .search .button");
-let apiKey = "RGAPI-d63846e6-934b-4765-b5a8-eab2b5105d9c";
+let apiKey = "RGAPI-a1f2cca0-6bfc-4afd-a3d0-719cde0bf958";
 let dataContainer = document.querySelector("main .container");
 let inputData = document.querySelector("header .container .search input");
 let skillsArray = [];
